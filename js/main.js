00132529ec687a37184c9d11a37eea8c3943ddd5
GemaMesasV <gema.mesas.v@gmail.com>
2022-03-18 11:22:19 +0100

    2.1 proyect session

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const form = document.querySelector('.js-new-form');
+
+form.classList.remove("collapsed");
+
+const catList = document.querySelector(".js-list");
+
+const card = document.querySelector(".card");
+
+card.innerHTML = "variable";
