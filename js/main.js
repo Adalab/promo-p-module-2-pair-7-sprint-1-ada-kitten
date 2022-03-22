76d3c0c48662059d831dafecd6f141b5bf9b1872
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-22 21:05:33 +0100

    Includes the Exercise 2

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
 "use strict";
 
-const form = document.querySelector('.js-new-form');
-
-form.classList.remove("collapsed");
+// const form = document.querySelector('.js-new-form');
+// form.classList.remove("collapsed");
 
 const catList = document.querySelector(".js-list");
 const kittenImageOne = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 const kittenNameOne = "Anastacio";
 const kittenDescOne =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  "(cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRaceOne = "British Shorthair";
 
 const kittenImageTwo = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
 const kittenNameTwo = "Fiona";
 const kittenDescTwo =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  "(cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRaceTwo = "British Shorthair";
 
 const kittenImageThree = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
 const kittenNameThree = "Cielo";
 const kittenDescThree =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  " (cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! cariñoso";
 const kittenRaceThree = "British Shorthair";
 
  
@@ -58,7 +57,44 @@ ${kittenDescThree}
 </p>
 </li>`;
 
-catList.innerHTML = kittenOne + kittenTwo + kittenThree;
+//catList.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+/* Parte 1} */
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+const descrSearchText = input_search_desc.value;
+
+if( kittenDescOne.includes(descrSearchText) ) {
+  catList.innerHTML = kittenOne;
+}
+
+if( kittenDescTwo.includes(descrSearchText) ) {
+  catList.innerHTML += kittenTwo;
+}
+
+if( kittenDescThree.includes(descrSearchText) ) {
+  catList.innerHTML += kittenThree;
+}
 
 
 
+/* Parte 2 */
+const formElement = document.querySelector('.js-new-form');
+if (formElement.classList.contains('collapsed')) {
+  formElement.classList.remove("collapsed");
+} else {
+  formElement.classList.add("collapsed");
+}
+
+// formElement.classList.toggle("collapsed");
+
+
+
+
+/* Bonus: No se ha especificado la raza, ¿qué se muestra entonces?
+ if (kittenRace1 === "")) {
+  html += `<h3 class='card_race'>No se ha especificado la raza</h3>`;
+} else {
+  ...
+}
+*/
