19c4069fc2c306ac3b3b145528ce685d2f33c907
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-24 22:24:32 +0100

    exercise 2.5 part two, three y four

--- js/main.js
+++ js/main.js
@@ -24,39 +24,60 @@ const kittenDescThree =
   " (cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! cariñoso";
 const kittenRaceThree = "British Shorthair";
 
-const kittenOne = `<li class="card"> <article><img
-    class="card_img"
-    src="${kittenImageOne}"
-    alt="gatito"
-  />
-  <h3 class="card_title"> ${kittenNameOne} </h3>
-  <h4 class="card_race">${kittenRaceOne}</h4>
-  <p class="card_description">
-  ${kittenDescOne}
-  </p>
-</article>
-</li>`;
+const kittenOne = renderKitten(
+  kittenImageOne,
+  kittenDescOne,
+  kittenNameOne,
+  kittenRaceOne
+);
 
-const kittenTwo = `<li class="card">
-<img class="card_img"
-  src="${kittenImageTwo}" />
-<h3 class="card_title">${kittenNameTwo}</h3>
-<h4 class="card_race">${kittenRaceTwo}</h4>
-<p class="card_description">
-${kittenDescTwo}
-</p>
-</li>`;
+const kittenTwo = renderKitten(
+  kittenImageTwo,
+  kittenDescTwo,
+  kittenNameTwo,
+  kittenRaceTwo
+);
 
-const kittenThree = `<li class="card">
-<img class="card_img"
-  src="${kittenImageThree}"
-  alt="gatito" />
-<h3 class="card_title">${kittenNameThree}</h3>
-<h4 class="card_race">${kittenRaceThree}</h4>
-<p class="card_description">
-${kittenDescThree}
-</p>
-</li>`;
+const kittenThree = renderKitten(
+  kittenImageThree,
+  kittenDescThree,
+  kittenNameThree,
+  kittenRaceThree
+);
+
+// const kittenOne = `<li class="card"> <article><img
+//     class="card_img"
+//     src="${kittenImageOne}"
+//     alt="gatito"
+//   />
+//   <h3 class="card_title"> ${kittenNameOne} </h3>
+//   <h4 class="card_race">${kittenRaceOne}</h4>
+//   <p class="card_description">
+//   ${kittenDescOne}
+//   </p>
+// </article>
+// </li>`;
+
+// const kittenTwo = `<li class="card">
+// <img class="card_img"
+//   src="${kittenImageTwo}" />
+// <h3 class="card_title">${kittenNameTwo}</h3>
+// <h4 class="card_race">${kittenRaceTwo}</h4>
+// <p class="card_description">
+// ${kittenDescTwo}
+// </p>
+// </li>`;
+
+// const kittenThree = `<li class="card">
+// <img class="card_img"
+//   src="${kittenImageThree}"
+//   alt="gatito" />
+// <h3 class="card_title">${kittenNameThree}</h3>
+// <h4 class="card_race">${kittenRaceThree}</h4>
+// <p class="card_description">
+// ${kittenDescThree}
+// </p>
+// </li>`;
 
 //catList.innerHTML = kittenOne + kittenTwo + kittenThree;
 
@@ -97,20 +118,21 @@ const inputDesc = document.querySelector(".js-input-desc");
 const inputPhoto = document.querySelector(".js-input-photo");
 const inputName = document.querySelector(".js-input-name");
 const labelMesageError = document.querySelector(".js-label-error");
-const btnAdd = document.querySelector(".js-btn-add");
+const buttonAdd = document.querySelector(".js-btn-add");
 
-btnAdd.addEventListener("click", (event) => {
-  // alert("Has hecho click en el botón");
-  const valueDesc = inputDesc.value;
-  const valuePhoto = inputPhoto.value;
-  const valueName = inputName.value;
+// buttonAdd.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   // alert("Has hecho click en el botón");
+//   const valueDesc = inputDesc.value;
+//   const valuePhoto = inputPhoto.value;
+//   const valueName = inputName.value;
 
-  if (valueDesc === "" || valuePhoto === "" || valueName === "") {
-    labelMesageError.innerHTML = "Debes rellenar todos los campos";
-  } else {
-    labelMesageError.innerHTML = "";
-  }
-});
+//   if (valueDesc === "" || valuePhoto === "" || valueName === "") {
+//     labelMesageError.innerHTML = "Debes rellenar todos los campos";
+//   } else {
+//     labelMesageError.innerHTML = "";
+//   }
+// });
 
 // Parte 2.4 ejercicio 3
 
@@ -128,24 +150,24 @@ btnSearch.addEventListener("click", (event) => {
 });
 
 // Parte 2.4 ejercicio 4
-/* const btnCancel = document.querySelector(".js-button-cancel");
+const buttonCancel = document.querySelector(".js-button-cancel");
 const newFormElement = document.querySelector(".js-new-form");
 const newFormInputPhoto = document.querySelector(".js-input-photo");
 const newFormInputName = document.querySelector(".js-input-name");
 const newFormInputRace = document.querySelector(".js-input-race");
 const newForminputDesc = document.querySelector(".js-input-desc");
 
-btnCancel.addEventListener("click", (event) => {
-  event.preventDefault();
-  newFormElement.classList.toggle("collapsed");
-  newFormInputPhoto.value = "";
-  newFormInputName.value = "";
-  newFormInputRace.value = "";
-  newForminputDesc.value = "";
-}); */
+// btnCancel.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   newFormElement.classList.toggle("collapsed");
+//   newFormInputPhoto.value = "";
+//   newFormInputName.value = "";
+//   newFormInputRace.value = "";
+//   newForminputDesc.value = "";
+// });
 
 //Parte 2.5 ejercicio 1
-const newFormElement = document.querySelector(".js-new-form");
+// const newFormElement = document.querySelector(".js-new-form");
 const link = document.querySelector(".js_click");
 
 function showNewCatForm() {
@@ -166,3 +188,52 @@ function handleClickNewCatForm(event) {
 }
 
 link.addEventListener("click", handleClickNewCatForm);
+
+//Parte 2.5 ejercicio 2
+
+function renderKitten(url, desc, name, race) {
+  return `<li class="card"> 
+    <article>
+      <img class="card_img"src="${url}" alt="gatito"/>
+      <h3 class="card_title"> ${name} </h3>
+      <h4 class="card_race">${race}</h4>
+      <p class="card_description">
+        ${desc}
+      </p>
+    </article> 
+  </li>`;
+}
+//linea 27 he cambiado el código //
+
+//Parte 2.5 ejercicio 3
+
+//modifica el evento para cumplir una función manejadora
+buttonAdd.addEventListener("click", addNewKitten);
+
+function addNewKitten(event) {
+  //mueve el código que está dentro del evento
+  event.preventDefault();
+  const valueDesc = inputDesc.value;
+  const valuePhoto = inputPhoto.value;
+  const valueName = inputName.value;
+
+  if (valueDesc === "" || valuePhoto === "" || valueName === "") {
+    labelMesageError.innerHTML = "Debes rellenar todos los campos";
+  } else {
+    labelMesageError.innerHTML = "";
+  }
+}
+
+//Parte 2.5 ejercicio 4
+buttonCancel.addEventListener("click", cancelNewKitten);
+
+function cancelNewKitten(event) {
+  event.preventDefault();
+  newFormElement.classList.toggle("collapsed");
+  newFormInputPhoto.value = "";
+  newFormInputName.value = "";
+  newFormInputRace.value = "";
+  newForminputDesc.value = "";
+}
+
+//Parte 2.5 ejercicio 5
