54e505ceaca1e88195b32ea5922172db667fb475
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-30 23:44:02 +0200

    added order of code Js and finished exercise 6 (objects)

--- js/main.js
+++ js/main.js
@@ -1,176 +1,69 @@
 "use strict";
 
-//Parte 2.1 ejercicio 1
-// const form = document.querySelector('.js-new-form');
-// form.classList.remove("collapsed");
-
-const catList = document.querySelector(".js-list");
-const kittenImageOne = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
-const kittenNameOne = "Anastacio";
-const kittenDescOne =
-  "(cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRaceOne = "British Shorthair";
-
-const kittenImageTwo =
-  "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg";
-const kittenNameTwo = "Fiona";
-const kittenDescTwo =
-  "(cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRaceTwo = "British Shorthair";
-
-const kittenImageThree =
-  "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
-const kittenNameThree = "Cielo";
-const kittenDescThree =
-  " (cariñoso) Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle! cariñoso";
-const kittenRaceThree = "British Shorthair";
-
-const kittenOne = renderKitten(
-  kittenImageOne,
-  kittenDescOne,
-  kittenNameOne,
-  kittenRaceOne
-);
-
-const kittenTwo = renderKitten(
-  kittenImageTwo,
-  kittenDescTwo,
-  kittenNameTwo,
-  kittenRaceTwo
-);
-
-const kittenThree = renderKitten(
-  kittenImageThree,
-  kittenDescThree,
-  kittenNameThree,
-  kittenRaceThree
-);
-//Parte 2.2 ejercicio 1
-// const kittenOne = `<li class="card"> <article><img
-//     class="card_img"
-//     src="${kittenImageOne}"
-//     alt="gatito"
-//   />
-//   <h3 class="card_title"> ${kittenNameOne} </h3>
-//   <h4 class="card_race">${kittenRaceOne}</h4>
-//   <p class="card_description">
-//   ${kittenDescOne}
-//   </p>
-// </article>
-// </li>`;
-
-// const kittenTwo = `<li class="card">
-// <img class="card_img"
-//   src="${kittenImageTwo}" />
-// <h3 class="card_title">${kittenNameTwo}</h3>
-// <h4 class="card_race">${kittenRaceTwo}</h4>
-// <p class="card_description">
-// ${kittenDescTwo}
-// </p>
-// </li>`;
-
-// const kittenThree = `<li class="card">
-// <img class="card_img"
-//   src="${kittenImageThree}"
-//   alt="gatito" />
-// <h3 class="card_title">${kittenNameThree}</h3>
-// <h4 class="card_race">${kittenRaceThree}</h4>
-// <p class="card_description">
-// ${kittenDescThree}
-// </p>
-// </li>`;
-
-//catList.innerHTML = kittenOne + kittenTwo + kittenThree;
-
-// Parte 2.3 ejercicio 1 Búsqueda por descripción
-// const input_search_desc = document.querySelector(".js_in_search_desc");
-// input_search_desc.value = "cariñoso";
-// const descrSearchText = input_search_desc.value;
-
-// if (kittenDescOne.includes(descrSearchText)) {
-//   catList.innerHTML = kittenOne;
-// }
-
-// if (kittenDescTwo.includes(descrSearchText)) {
-//   catList.innerHTML += kittenTwo;
-// }
-
-// if (kittenDescThree.includes(descrSearchText)) {
-//   catList.innerHTML += kittenThree;
-// }
-
-// Parte 2.3 ejercicio 2 Mostrar/ocultar el formulario añadir
-/* const formElement = document.querySelector(".js-new-form");
-if (formElement.classList.contains("collapsed")) {
-  formElement.classList.remove("collapsed");
-} else {
-  formElement.classList.add("collapsed");
-} */
-
-// Parte 2.4 ejercicio 1 Mostrar/ocultar formulario y 2 Validar formulario nuevo gatito
-
-/* const clickButton = document.querySelector(".js_click");
-
-clickButton.addEventListener("click", (event) => {
-  formElement.classList.toggle("collapsed");
-}); */
-
+/* Elements from HTML */
+const newFormElement = document.querySelector(".js-new-form");
+const listElement = document.querySelector(".js-list");
+const searchButton = document.querySelector(".js-button-search");
+const buttonAdd = document.querySelector(".js-btn-add");
+const buttonCancelForm = document.querySelector(".js-btn-cancel");
 const inputDesc = document.querySelector(".js-input-desc");
 const inputPhoto = document.querySelector(".js-input-photo");
 const inputName = document.querySelector(".js-input-name");
+const linkNewFormElememt = document.querySelector(".js-button-new-form");
 const labelMesageError = document.querySelector(".js-label-error");
-const buttonAdd = document.querySelector(".js-btn-add");
+const input_search_desc = document.querySelector(".js_in_search_desc");
 
-// buttonAdd.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   // alert("Has hecho click en el botón");
-//   const valueDesc = inputDesc.value;
-//   const valuePhoto = inputPhoto.value;
-//   const valueName = inputName.value;
+//Objects from Kittens
+const kittenData_1 = {
+  image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+  name: "Anastacio",
+  desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
+const kittenData_2 = {
+  image:
+    "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+  name: "Fiona",
+  desc: "Juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
+const kittenData_3 = {
+  image:
+    "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+  name: "Cielo",
+  desc: "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
 
-//   if (valueDesc === "" || valuePhoto === "" || valueName === "") {
-//     labelMesageError.innerHTML = "Debes rellenar todos los campos";
-//   } else {
-//     labelMesageError.innerHTML = "";
-//   }
-// });
+const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
 
-// Parte 2.4 ejercicio 3 Validar formulario búsqueda
-/* 
-const inputDescSearch = document.querySelector(".js_in_search_desc");
+//Funtions
+function renderKitten(kittenData) {
+  const kitten = `<li class="card">
+    <article>
+      <img
+        class="card_img"
+        src=${kittenData.image}
+        alt="gatito"
+      />
+      <h3 class="card_title">${kittenData.name}</h3>
+      <h3 class="card_race">${kittenData.race}</h3>
+      <p class="card_description">
+      ${kittenData.desc}
+      </p>
+    </article>
+    </li>`;
+  return kitten;
+}
 
-const btnSearch = document.querySelector(".js_search");
-btnSearch.addEventListener("click", (event) => {
-  const valueDescSearch = inputDescSearch.value;
-
-  if (valueDescSearch === "") {
-    ("No hay descripción para la búsqueda");
-  } else {
-    console.log("puede buscar");
+function renderKittenList(kittenDataList) {
+  listElement.innerHTML = "";
+  for (const kittenItem of kittenDataList) {
+    listElement.innerHTML += renderKitten(kittenItem);
   }
-}); */
+}
 
-// Parte 2.4 ejercicio 4 Cancelar formulario
-const buttonCancel = document.querySelector(".js-button-cancel");
-const newFormElement = document.querySelector(".js-new-form");
-const newFormInputPhoto = document.querySelector(".js-input-photo");
-const newFormInputName = document.querySelector(".js-input-name");
-const newFormInputRace = document.querySelector(".js-input-race");
-const newForminputDesc = document.querySelector(".js-input-desc");
-
-// btnCancel.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   newFormElement.classList.toggle("collapsed");
-//   newFormInputPhoto.value = "";
-//   newFormInputName.value = "";
-//   newFormInputRace.value = "";
-//   newForminputDesc.value = "";
-// });
-
-//Parte 2.5 ejercicio 1 Mostrar/ocultar el formulario nuevo gatito
-// const newFormElement = document.querySelector(".js-new-form");
-const link = document.querySelector(".js_click");
-
+//Show/hide the form
 function showNewCatForm() {
   newFormElement.classList.remove("collapsed");
 }
@@ -180,144 +73,51 @@ function hideNewCatForm() {
 
 function handleClickNewCatForm(event) {
   event.preventDefault();
-  console.log("hola");
   if (newFormElement.classList.contains("collapsed")) {
-    newFormElement.classList.remove("collapsed");
+    showNewCatForm();
   } else {
-    newFormElement.classList.add("collapsed");
+    hideNewCatForm();
   }
 }
-
-link.addEventListener("click", handleClickNewCatForm);
-
-//Parte 2.5 ejercicio 2 Crear el gatito en HTML
-
-/* function renderKitten(url, desc, name, race) {
-  return `<li class="card"> 
-    <article>
-      <img class="card_img"src="${url}" alt="gatito"/>
-      <h3 class="card_title"> ${name} </h3>
-      <h4 class="card_race">${race}</h4>
-      <p class="card_description">
-        ${desc}
-      </p>
-    </article> 
-  </li>`;
-} */
-
-//Parte 2.5 ejercicio 3 Adicionar nuevo gatito
-
-//modifica el evento para cumplir una función manejadora
-buttonAdd.addEventListener("click", addNewKitten);
-
+//Add new kitten
 function addNewKitten(event) {
-  //mueve el código que está dentro del evento
   event.preventDefault();
   const valueDesc = inputDesc.value;
   const valuePhoto = inputPhoto.value;
   const valueName = inputName.value;
-
-  if (valueDesc === "" || valuePhoto === "" || valueName === "") {
-    labelMesageError.innerHTML = "Debes rellenar todos los campos";
+  if (valueDesc === "" && valuePhoto === "" && valueName === "") {
+    labelMesageError.innerHTML = "Debe rellenar todos los valores";
   } else {
-    labelMesageError.innerHTML = "";
+    if (valueDesc !== "" && valuePhoto !== "" && valueName !== "") {
+      labelMesageError.innerHTML = "";
+    }
   }
 }
-
-//Parte 2.5 ejercicio 4 Cancelar nuevo gatito
-buttonCancel.addEventListener("click", cancelNewKitten);
-
+//Cancel the search for a kitten
 function cancelNewKitten(event) {
   event.preventDefault();
-  newFormElement.classList.toggle("collapsed");
-  newFormInputPhoto.value = "";
-  newFormInputName.value = "";
-  newFormInputRace.value = "";
-  newForminputDesc.value = "";
+  newFormElement.classList.add("collapsed");
+  inputDesc.value = "";
+  inputPhoto.value = "";
+  inputName.value = "";
 }
 
-//Parte 2.5 ejercicio 5 Filtrar por descripción
-
-const input_search_desc = document.querySelector(".js_in_search_desc");
-
-const buttonSearch = document.querySelector(".js-button-search");
-buttonSearch.addEventListener("click", filterKitten);
-
+//Filter by description
 function filterKitten(event) {
   event.preventDefault();
-  catList.innerHTML = "";
   const descrSearchText = input_search_desc.value;
-  if (kittenDescOne.includes(descrSearchText)) {
-    catList.innerHTML += kittenOne;
-  }
-  if (kittenDescTwo.includes(descrSearchText)) {
-    catList.innerHTML += kittenTwo;
-  }
-  if (kittenDescThree.includes(descrSearchText)) {
-    catList.innerHTML += kittenThree;
+  listElement.innerHTML = "";
+  for (const kittenItem of kittenDataList) {
+    if (kittenItem.desc.includes(descrSearchText)) {
+      listElement.innerHTML += renderKitten(kittenItem);
+    }
   }
 }
-//Parte 2.5 ejercicio 6 Mostrar la raza o no
-/* 
-const input_search_race = document.querySelector(".js_in_search_race");
+//Show kitty list in HTML
+renderKittenList(kittenDataList);
 
-function renderRace(event) {
-  event.preventDefault();
-  const raceSearchText = input_search_race.value;
-  console.log("hola");
-  if (kittenRaceOne.includes(raceSearchText)) {
-    catList.innerHTML += kittenOne;
-  }
-  if (kittenRaceTwo.includes(raceSearchText)) {
-    catList.innerHTML += kittenTwo;
-  }
-  if (kittenRaceThree.includes(raceSearchText)) {
-    catList.innerHTML += kittenThree;
-  }
-} */
-/* return `<h3 class="card"> 
-<article>
-  <img class="card_img"src="${url}" alt="gatito"/>
-  <h3 class="card_title"> ${name} </h3>
-  <h4 class="card_race">${race}</h4>
-  <p class="card_description">
-    ${desc}
-  </p>
-</article> 
-</h3>`; */
-
-//Parte 2.6 ejercicio 1 Convertir cada gatito en un objeto
-
-const kittenData_1 = {
-  image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
-  name: "Anastacio",
-  desc: "Ruiseño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-  race: "British Shorthair",
-};
-const kittenData_2 = {
-  image:
-    "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
-  name: "Fiona",
-  desc: "Ruiseño, juguetón, cariñoso le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-  race: "British Shorthair",
-};
-const kittenData_3 = {
-  image:
-    "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
-  name: "Cielo",
-  desc: "Ruiseño, juguetón, cariñoso le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
-  race: "British Shorthair",
-};
-
-function renderKitten(kittenData) {
-  return `<li class="card"> 
-  <article>
-    <img class="card_img"src="${url}" alt="gatito"/>
-    <h3 class="card_title"> ${name} </h3>
-    <h4 class="card_race">${race}</h4>
-    <p class="card_description">
-      ${desc}
-    </p>
-  </article> 
-</li>`;
-}
+//Events
+linkNewFormElememt.addEventListener("click", handleClickNewCatForm);
+searchButton.addEventListener("click", filterKitten);
+buttonAdd.addEventListener("click", addNewKitten);
+buttonCancelForm.addEventListener("click", cancelNewKitten);
