d08fc142fddb91147cfc5a167c2a55c0482ddce2
NaiSaratxaga <nsaratxaga@gmail.com>
2022-03-30 09:57:36 +0200

    exercise 2.6/1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Parte 2.1 ejercicio 1
 // const form = document.querySelector('.js-new-form');
 // form.classList.remove("collapsed");
 
@@ -44,7 +45,7 @@ const kittenThree = renderKitten(
   kittenNameThree,
   kittenRaceThree
 );
-
+//Parte 2.2 ejercicio 1
 // const kittenOne = `<li class="card"> <article><img
 //     class="card_img"
 //     src="${kittenImageOne}"
@@ -81,7 +82,7 @@ const kittenThree = renderKitten(
 
 //catList.innerHTML = kittenOne + kittenTwo + kittenThree;
 
-// Parte 2.3
+// Parte 2.3 ejercicio 1 Búsqueda por descripción
 // const input_search_desc = document.querySelector(".js_in_search_desc");
 // input_search_desc.value = "cariñoso";
 // const descrSearchText = input_search_desc.value;
@@ -98,7 +99,7 @@ const kittenThree = renderKitten(
 //   catList.innerHTML += kittenThree;
 // }
 
-// Parte 2.3
+// Parte 2.3 ejercicio 2 Mostrar/ocultar el formulario añadir
 /* const formElement = document.querySelector(".js-new-form");
 if (formElement.classList.contains("collapsed")) {
   formElement.classList.remove("collapsed");
@@ -106,7 +107,7 @@ if (formElement.classList.contains("collapsed")) {
   formElement.classList.add("collapsed");
 } */
 
-// Parte 2.4 ejercicio 1 y 2
+// Parte 2.4 ejercicio 1 Mostrar/ocultar formulario y 2 Validar formulario nuevo gatito
 
 /* const clickButton = document.querySelector(".js_click");
 
@@ -134,8 +135,8 @@ const buttonAdd = document.querySelector(".js-btn-add");
 //   }
 // });
 
-// Parte 2.4 ejercicio 3
-
+// Parte 2.4 ejercicio 3 Validar formulario búsqueda
+/* 
 const inputDescSearch = document.querySelector(".js_in_search_desc");
 
 const btnSearch = document.querySelector(".js_search");
@@ -143,13 +144,13 @@ btnSearch.addEventListener("click", (event) => {
   const valueDescSearch = inputDescSearch.value;
 
   if (valueDescSearch === "") {
-    alert("No hay descripción para la búsqueda");
+    ("No hay descripción para la búsqueda");
   } else {
     console.log("puede buscar");
   }
-});
+}); */
 
-// Parte 2.4 ejercicio 4
+// Parte 2.4 ejercicio 4 Cancelar formulario
 const buttonCancel = document.querySelector(".js-button-cancel");
 const newFormElement = document.querySelector(".js-new-form");
 const newFormInputPhoto = document.querySelector(".js-input-photo");
@@ -166,7 +167,7 @@ const newForminputDesc = document.querySelector(".js-input-desc");
 //   newForminputDesc.value = "";
 // });
 
-//Parte 2.5 ejercicio 1
+//Parte 2.5 ejercicio 1 Mostrar/ocultar el formulario nuevo gatito
 // const newFormElement = document.querySelector(".js-new-form");
 const link = document.querySelector(".js_click");
 
@@ -189,9 +190,9 @@ function handleClickNewCatForm(event) {
 
 link.addEventListener("click", handleClickNewCatForm);
 
-//Parte 2.5 ejercicio 2
+//Parte 2.5 ejercicio 2 Crear el gatito en HTML
 
-function renderKitten(url, desc, name, race) {
+/* function renderKitten(url, desc, name, race) {
   return `<li class="card"> 
     <article>
       <img class="card_img"src="${url}" alt="gatito"/>
@@ -202,10 +203,9 @@ function renderKitten(url, desc, name, race) {
       </p>
     </article> 
   </li>`;
-}
-//linea 27 he cambiado el código //
+} */
 
-//Parte 2.5 ejercicio 3
+//Parte 2.5 ejercicio 3 Adicionar nuevo gatito
 
 //modifica el evento para cumplir una función manejadora
 buttonAdd.addEventListener("click", addNewKitten);
@@ -224,7 +224,7 @@ function addNewKitten(event) {
   }
 }
 
-//Parte 2.5 ejercicio 4
+//Parte 2.5 ejercicio 4 Cancelar nuevo gatito
 buttonCancel.addEventListener("click", cancelNewKitten);
 
 function cancelNewKitten(event) {
@@ -236,19 +236,16 @@ function cancelNewKitten(event) {
   newForminputDesc.value = "";
 }
 
-//Parte 2.5 ejercicio 5
-
-
+//Parte 2.5 ejercicio 5 Filtrar por descripción
 
 const input_search_desc = document.querySelector(".js_in_search_desc");
 
- 
-  const buttonSearch = document.querySelector(".js-button-search");
-  buttonSearch.addEventListener("click", filterKitten);
-
+const buttonSearch = document.querySelector(".js-button-search");
+buttonSearch.addEventListener("click", filterKitten);
 
 function filterKitten(event) {
   event.preventDefault();
+  catList.innerHTML = "";
   const descrSearchText = input_search_desc.value;
   if (kittenDescOne.includes(descrSearchText)) {
     catList.innerHTML += kittenOne;
@@ -259,4 +256,68 @@ function filterKitten(event) {
   if (kittenDescThree.includes(descrSearchText)) {
     catList.innerHTML += kittenThree;
   }
+}
+//Parte 2.5 ejercicio 6 Mostrar la raza o no
+/* 
+const input_search_race = document.querySelector(".js_in_search_race");
+
+function renderRace(event) {
+  event.preventDefault();
+  const raceSearchText = input_search_race.value;
+  console.log("hola");
+  if (kittenRaceOne.includes(raceSearchText)) {
+    catList.innerHTML += kittenOne;
+  }
+  if (kittenRaceTwo.includes(raceSearchText)) {
+    catList.innerHTML += kittenTwo;
+  }
+  if (kittenRaceThree.includes(raceSearchText)) {
+    catList.innerHTML += kittenThree;
+  }
+} */
+/* return `<h3 class="card"> 
+<article>
+  <img class="card_img"src="${url}" alt="gatito"/>
+  <h3 class="card_title"> ${name} </h3>
+  <h4 class="card_race">${race}</h4>
+  <p class="card_description">
+    ${desc}
+  </p>
+</article> 
+</h3>`; */
+
+//Parte 2.6 ejercicio 1 Convertir cada gatito en un objeto
+
+const kittenData_1 = {
+  image: "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg",
+  name: "Anastacio",
+  desc: "Ruiseño, juguetón, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
+const kittenData_2 = {
+  image:
+    "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg",
+  name: "Fiona",
+  desc: "Ruiseño, juguetón, cariñoso le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
+const kittenData_3 = {
+  image:
+    "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg",
+  name: "Cielo",
+  desc: "Ruiseño, juguetón, cariñoso le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!",
+  race: "British Shorthair",
+};
+
+function renderKitten(kittenData) {
+  return `<li class="card"> 
+  <article>
+    <img class="card_img"src="${url}" alt="gatito"/>
+    <h3 class="card_title"> ${name} </h3>
+    <h4 class="card_race">${race}</h4>
+    <p class="card_description">
+      ${desc}
+    </p>
+  </article> 
+</li>`;
 }
